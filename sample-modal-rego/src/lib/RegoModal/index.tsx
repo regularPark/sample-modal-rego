746aaea9cdb36bef1743ef9bbd16f1df9c98abd2
regularPark <srk99279@naver.com>
2023-05-02 15:59:03 +0900

    feat: 0.0.1버전 생성

---
+++ sample-modal-rego/src/lib/RegoModal/index.tsx
@@ -0,0 +1,61 @@
+import styled from "styled-components";
+import React, { ReactElement, ReactNode, useState } from "react";
+
+type Props = {
+  trigger: ReactElement;
+  children: ReactNode;
+};
+
+export function RegoModal({ trigger, children }: Props) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+  return (
+    <>
+      {React.cloneElement(trigger, { onClick: handleOpenModal })}
+      {isModalOpen && (
+        <S.RegoModalOverlay onClick={handleCloseModal}>
+          <S.RegoModal onClick={(e) => e.stopPropagation()}>
+            {children}
+          </S.RegoModal>
+        </S.RegoModalOverlay>
+      )}
+    </>
+  );
+}
+
+const S = {
+  RegoModalOverlay: styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `,
+  RegoModal: styled.div`
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: white;
+    border-radius: 10px;
+    padding: 20px;
+    width: 500px; /* 수정 */
+    height: 500px; /* 수정 */
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+  `,
+};
---
+++ sample-modal-rego/src/lib/index.tsx
@@ -0,0 +1,1 @@
+export { RegoModal } from "../lib/RegoModal";

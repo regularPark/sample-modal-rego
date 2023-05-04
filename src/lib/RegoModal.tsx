088b66765c79d00a091304905f7c064edfba312b
regularPark <srk99279@naver.com>
2023-05-04 17:48:53 +0900

    feat: 0.0.6 버전 업데이트

--- src/lib/RegoModal.tsx
+++ src/lib/RegoModal.tsx
@@ -1,60 +1,23 @@
-import styled from "styled-components";
-import React, { PropsWithChildren, ReactElement, useState } from "react";
+import { Styled as S } from "./RegoModal.styles";
+import ReactDOM from "react-dom";
 
-type Props = {
-  trigger: ReactElement;
+export type ModalProps = {
+  isModalOpen: boolean;
+  closeModal: () => void;
+  children: JSX.Element | JSX.Element[];
 };
 
-export const RegoModal = ({ trigger, children }: PropsWithChildren<Props>) => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const handleOpenModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const handleCloseModal = () => {
-    setIsModalOpen(false);
-  };
-  return (
-    <>
-      {React.cloneElement(trigger, { onClick: handleOpenModal })}
-      {isModalOpen && (
-        <S.RegoModalOverlay onClick={handleCloseModal}>
-          <S.RegoModal onClick={(e) => e.stopPropagation()}>
-            {children}
-          </S.RegoModal>
-        </S.RegoModalOverlay>
-      )}
-    </>
+export function RegoModal({ isModalOpen, closeModal, children }: ModalProps) {
+  return ReactDOM.createPortal(
+    isModalOpen && (
+      <>
+        <S.ModalBackdrop onClick={closeModal}></S.ModalBackdrop>
+        <S.Modal className={"modal"}>
+          {children}
+          <S.ModalCloseButton onClick={closeModal}>X</S.ModalCloseButton>
+        </S.Modal>
+      </>
+    ),
+    document.querySelector("body") as HTMLBodyElement
   );
-};
-
-const S = {
-  RegoModalOverlay: styled.div`
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background: rgba(0, 0, 0, 0.5);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  `,
-  RegoModal: styled.div`
-    position: fixed;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    background-color: white;
-    border-radius: 10px;
-    padding: 20px;
-    width: 500px; /* 수정 */
-    height: 500px; /* 수정 */
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    text-align: center;
-  `,
-};
+}

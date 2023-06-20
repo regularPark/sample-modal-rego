af1502b26cd59873ef3594fb30ec30e76e1dcfa0
regularPark <srk99279@naver.com>
2023-06-20 23:20:49 +0900

    feat: add Close Button with react-icons

--- src/lib/RegoModal.tsx
+++ src/lib/RegoModal.tsx
@@ -1,23 +1,26 @@
-import { Styled as S } from "./RegoModal.styles";
-import ReactDOM from "react-dom";
+import { Styled as S } from './RegoModal.styles'
+import ReactDOM from 'react-dom'
+import { IoIosClose } from 'react-icons/io'
 
 export type ModalProps = {
-  isModalOpen: boolean;
-  closeModal: () => void;
-  children: JSX.Element | JSX.Element[];
-};
+  isModalOpen: boolean
+  closeModal: () => void
+  children: JSX.Element | JSX.Element[]
+}
 
 export function RegoModal({ isModalOpen, closeModal, children }: ModalProps) {
   return ReactDOM.createPortal(
     isModalOpen && (
       <>
         <S.ModalBackdrop onClick={closeModal}></S.ModalBackdrop>
-        <S.Modal className={"modal"}>
+        <S.Modal className={'modal'}>
           {children}
-          <S.ModalCloseButton onClick={closeModal}>X</S.ModalCloseButton>
+          <S.ModalCloseButton onClick={closeModal}>
+            <IoIosClose />
+          </S.ModalCloseButton>
         </S.Modal>
       </>
     ),
-    document.querySelector("body") as HTMLBodyElement
-  );
+    document.querySelector('body') as HTMLBodyElement,
+  )
 }
